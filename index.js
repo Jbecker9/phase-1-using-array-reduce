4a135f1b70921f44c6de4447948a40a40926651f
Jared <jbeckersalax9@aol.com>
2021-12-10 12:46:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const { arrayContaining } = require("expect");
-
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; // Provided Code
 
-function reducer(arr, num){
-    return arr.reduce(x => x + arr)
+function totalBatteries(batteryBatches, num){
+    return batteryBatches.reduce(x => x.sum)
 }
